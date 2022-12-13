dc6c9136dc96fed12661ea85908c57a3daa91fbe
Napatmak <117625172+Napatmak@users.noreply.github.com>
2022-12-13 16:18:13 +0700

    Update app.py

--- app.py
+++ app.py
@@ -12,11 +12,11 @@ def chainsaw_man():
 
 @app.route("/86_Eighty_Six") #2
 def eighty_six():
-    return render_template("86 Eighty Six.html")
+    return render_template("86_Eighty_Six.html")
 
 @app.route("/Black_Clover") #3
 def black_clover():
-    return render_template("Black Clover.html")
+    return render_template("Black_Clover.html")
 
 @app.route("/Bleach") #4
 def bleach():
@@ -24,35 +24,35 @@ def bleach():
 
 @app.route("/Dr._Stone") #5 
 def dr_stone():
-    return render_template("Dr. Stone.html")
+    return render_template("Dr._Stone.html")
 
 @app.route("/Fire_Force") #6
 def fire_force():
-    return render_template("Fire Force.html")
+    return render_template("Fire_Force.html")
 
 @app.route("/Fullmetal_Alchemist_Brotherhood") #7 
 def fullmetal_alchemist_brotherhood():
-    return render_template("Fullmetal Alchemist Brotherhood.html")
+    return render_template("Fullmetal_Alchemist_Brotherhood.html")
 
 @app.route("/Kaguya-sama_wa_Kokurasetai") #8
 def kaguya_sama_wa_kokurasetai():
-    return render_template("Kaguya-sama wa Kokurasetai.html")
+    return render_template("Kaguya-sama_wa_Kokurasetai.html")
 
 @app.route("/Made_in_Abyss") #9
 def made_in_abyss():
-    return render_template("Made in Abyss.html")
+    return render_template("Made_in_Abyss.html")
 
 @app.route("/Mononoke_Hime") #10
 def mononoke_hime():
-    return render_template("Mononoke Hime.html")
+    return render_template("Mononoke_Hime.html")
 
 @app.route("/Mushishi_Zoku_Shou") #11 
 def mushishi_zoku_shou():
-    return render_template("Mushishi Zoku Shou.html")
+    return render_template("Mushishi_Zoku_Shou.html")
 
 @app.route("/Mushoku_Tensei") #12
 def mushoku_tensei():
-    return render_template("Mushoku Tensei.html")
+    return render_template("Mushoku_Tensei.html")
 
 @app.route("/Nichijou") #13
 def nichijou():
@@ -60,15 +60,15 @@ def nichijou():
 
 @app.route("/Platinum_End") #14 
 def platinum_end():
-    return render_template("Platinum End.html")
+    return render_template("Platinum_End.html")
 
 @app.route("/Spy_x_Family") #15
 def spy_x_family():
-    return render_template("Spy x Family.html")
+    return render_template("Spy_x_Family.html")
 
 @app.route("/Steins;Gate") #16
 def steins_gate():
-    return render_template("Steins;Gate.html")
+    return render_template("SteinsGate.html")
 
 @app.route("/Cross_Game") #17
 def cross_game():
@@ -84,7 +84,7 @@ def haikyu():
 
 @app.route("/Kimitsu_No_Yaiba") #20
 def kimitsu_no_yaiba():
-    return render_template("kimitsu no yaiba.html")
+    return render_template("kimitsu_no_yaiba.html")
 
 @app.route("/Owarimonogatari") #21
 def owarimonogatari():
@@ -92,35 +92,35 @@ def owarimonogatari():
 
 @app.route("/Hunter_x_Hunter") #22
 def hunter_x_hunter():
-    return render_template("Hunter x Hunter.html") 
+    return render_template("Hunter_x_Hunter.html") 
 
 @app.route("/Jujutsu_Kaisen") #23
 def jujutsu_kaisen():
-    return render_template("Jujutsu Kaisen.html") 
+    return render_template("Jujutsu_Kaisen.html") 
 
 @app.route("/Koe_no_Katachi") #24
 def koe_no_katachi():
-    return render_template("Koe no Katachi.html") 
+    return render_template("Koe_no_Katachi.html") 
 
 @app.route("/Legend_of_the_Galactic_Heroes") #25 
 def legend_of_the_galactic_heroes():
-    return render_template("Legend of the Galactic Heroes.html") 
+    return render_template("Legend_of_the_Galactic_Heroes.html") 
 
 @app.route("/Natsume_Yuujinchou_Roku") #26
 def natsume_yuujinchou_roku():
-    return render_template("Natsume Yuujinchou Roku.html") 
+    return render_template("Natsume_Yuujinchou_Roku.html") 
     
 @app.route("/Ookami_Kodomo_no_Ame_to_Yuki") #27 
 def ookami_kodomo_no_ame_to_yuki():
-    return render_template("Ookami Kodomo no Ame to Yuki.html") 
+    return render_template("Ookami_Kodomo_no_Ame_to_Yuki.html") 
 
 @app.route("/Shigatsu_wa_Kimi_no_Uso") #28
 def shigatsu_wa_kimi_no_uso():
-    return render_template("Shigatsu wa Kimi no Uso.html") 
+    return render_template("Shigatsu_wa_Kimi_no_Uso.html") 
 
 @app.route("/Summertime_Render") #29
 def summertime_render():
-    return render_template("Summertime Render.html") 
+    return render_template("Summertime_Render.html") 
 
 @app.route("/Suzumiya_Haruhi_no_Shoushitsu") #30 
 def suzumiya_haruhi_no_shoushitsu():
@@ -128,7 +128,7 @@ def suzumiya_haruhi_no_shoushitsu():
 
 @app.route("/Tengen_Toppa_Gurren_Lagann") #31
 def tengen_toppagurren_lagann():
-    return render_template("Tengen Toppa Gurren Lagann.html") 
+    return render_template("Tengen_Toppa_Gurren_Lagann.html") 
 
 @app.route("/Uchuu_kyoudai") #32
 def uchuu_kyoudai():
@@ -136,7 +136,7 @@ def uchuu_kyoudai():
 
 @app.route("/Zoku_Natsume_Yuujinchou") #33 
 def zoku_natsume_yuujinchou():
-    return render_template("Zoku Natsume Yuujinchou.html") 
+    return render_template("Zoku_Natsume_Yuujinchou.html") 
 
 @app.route("/Aria_The_Origination") #34
 def aria_the_origination():
@@ -156,7 +156,7 @@ def cpde_geass():
 
 @app.route("/Hotaru_no_Haka") #38
 def hotaru_no_haka():
-    return render_template("Hotaru no Haka.html") 
+    return render_template("Hotaru_no_Haka.html") 
        
 @app.route("/Kimi_no_Na_Wa") #39
 def kimi_no_na_wa():
@@ -168,31 +168,31 @@ def violet_evergarden_movie():
 
 @app.route("/Yakusoku_no_Neverland") #41
 def yakusoku_no_neverland():
-    return render_template("Yakusoku no Neverland.html") 
+    return render_template("Yakusoku_no_Neverland.html") 
 
 @app.route("/Perfect_Blue") #42
 def perfect_blue():
-    return render_template("Perfect Blue.html") 
+    return render_template("Perfect_Blue.html") 
 
 @app.route("/One_Punch_Man") #43
 def one_punch_mna():
-    return render_template("One Punch Man.html") 
+    return render_template("One_Punch_Man.html") 
 
 @app.route("/Slam_Dunk") #44
 def slam_dunk():
-    return render_template("Slam Dunk.html") 
+    return render_template("Slam_Dunk.html") 
 
 @app.route("/Samurai_Champloo") #45
 def samurai_champloo():
-    return render_template("Samurai Champloo.html") 
+    return render_template("Samurai_Champloo.html") 
 
 @app.route("/Sora_yori_mo_Tooi_Basho") #46
 def sora_yori_mo_tooi_basho():
-    return render_template("Sora yori mo Tooi Basho.html") 
+    return render_template("Sora_yori_mo_Tooi_Basho.html") 
 
 @app.route("/Kenpuu_Denki_Berserk") #47
 def kenpuu_denki_berserk():
-    return render_template("Kenpuu Denki Berserk.html") 
+    return render_template("Kenpuu_Denki_Berserk.html") 
 
 @app.route("/Gintama") #48
 def gintama():
@@ -200,11 +200,11 @@ def gintama():
 
 @app.route("/Attack_on_Titan") #49
 def attack_on_titan():
-    return render_template("Attack on Titan.html") 
+    return render_template("Attack_on_Titan.html") 
 
 @app.route("/Kimi_no_Suizou_wo_Tabetai") #50
 def kimi_no_suizou_wo_tabetai():
-    return render_template("Kimi no Suizou wo Tabetai.html") 
+    return render_template("Kimi_no_Suizou_wo_Tabetai.html") 
 
 @app.route("/Sci-Fi") #random scifi
 def sci_fi():
@@ -220,15 +220,15 @@ def action():
 
 @app.route("/Mystery") #random horror
 def mystery():
-    return render_template("mytery.html") 
+    return render_template("mystery.html") 
 
 @app.route("/Drama") #random drama
 def drama():
     return render_template("drama.html") 
 
-@app.route("/Super_Natural") #random super natural
+@app.route("/super_natural") #random super natural
 def super_natural():
-    return render_template("super natural.html") 
+    return render_template("super_natural.html") 
 
 @app.route("/Comedy") #random comedy
 def comedy():
@@ -248,4 +248,4 @@ def romance():
 
 @app.route("/Slice_Of_Life") #random slice of life
 def slice_of_life():
-    return render_template("slice_of_life.html") 
+    return render_template("slice_of_life.html")
